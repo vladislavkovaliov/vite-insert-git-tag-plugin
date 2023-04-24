8c628683e0a38703018b5d3f1f774a8ce57e92b3
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2023-04-24 17:01:10 +0300

    chore: updates dist

--- dist/plugin/InsertGitTag.js
+++ dist/plugin/InsertGitTag.js
@@ -18,7 +18,7 @@ function viteInsertVersion() {
                 await execAsync("git fetch --tags");
                 const { stdout } = await execAsync("git describe --tags --abbrev=0");
                 const newHtml = html.substring(0, index + len) +
-                    `<meta content="${stdout.trim()}">` +
+                    `<meta name="version" content="${stdout.trim()}">` +
                     html.substring(index + len);
                 return newHtml;
             }
